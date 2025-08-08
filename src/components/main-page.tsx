289c3f00953872c52b02d01035ebcb894198d1c6
Donita Lemek <donitalemek@gmail.com>
2025-08-08 21:23:18 +0000

    load roast

--- src/components/main-page.tsx
+++ src/components/main-page.tsx
@@ -271,7 +271,7 @@ export function MainPage() {
             </div>
 
             <AnimatePresence>
-                {(isRoasting || roast) && (
+                {(analysisResult) && (
                   <motion.div initial={{ opacity: 0, y: 10 }} animate={{ opacity: 1, y: 0 }}>
                     <Card>
                       <CardHeader>
